5c64fc7dc64a3098a57ce5f7c94d7f0082cc47b9
LBIV4 <lanebartoniv@gmail.com>
2019-09-09 15:40:56 -0700

    Success with first API

--- data_processing/apis.js
+++ data_processing/apis.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 /**
  * Method to create cookies based on passed in key-value pairs and league privacy
- * @param {*} keyValuePairs Json representing additional key-value pairs to add 
+ * @param {Object<JSON>} keyValuePairs Json representing additional key-value pairs to add 
  */
 const get_cookies = (keyValuePairs) => {
     let output = "";
@@ -27,8 +27,8 @@ async function get_data(url, headers, params) {
         responseType: 'json'
     }
    return await axios(options)
-        .then((data) => {
-            return data;
+        .then((resp) => {
+            return resp.data;
         })
         .catch((err) => {
             console.log(`Error processing request: ${err}`);
@@ -36,17 +36,39 @@ async function get_data(url, headers, params) {
         })
 }
 
+const slotIdToPos = (slotId) => {
+    const map = {
+        0: "QB",
+        2: "RB",
+        3: "RB/WR",
+        4: "WR",
+        6: "TE",
+        16: "D/ST",
+        17: "K",
+        20: "BE",
+        23: "FLEX"
+    }
+    return map[slotId];
+}
+
+/**
+ * Method to get scoreboard information
+ * @param {int} year 
+ * @param {int?} scoringPeriod 
+ */
 const get_scoreboard = async (year, scoringPeriod) => {
     const uri = `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${year}/segments/0/leagues/454525`
-    //const views = ["modular", "mNav", "mMatchupScore", "mRoster", "mScoreboard", "mSettings", "mTopPerformers", "mTeam"];
-    const views = ["mMatchupScore"]
+    //const views = ["modular", "mNav", "mMatchupScore", "mRoster", "mScoreboard", "mSettings", "mTopPerformers", "mTeam", "mPositionalRatings", "kona_player_info"];
+    const views = ["mScoredboard"]
     const data = {
-        scoringPeriod: scoringPeriod,
         view: views.join(",")
     }
     const output = await get_data(uri, null, data);
-    if (output) {
-        console.log(output.data)
+    const scoringPeriod = scoringPeriod || output.scoringPeriodId;
+    if (!output) {
+        console.log("Cannot find data for year " + year)
+    } else {
+        console.log("Found data");
     }
 }
 
